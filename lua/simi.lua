b0f6b3d14d74de213eea8c376611d5d7c17b7222
Em1lT <emilanton.toivainen@gmail.com>
2022-10-25 16:27:05 +0300

    feat: add search name under cursor func

--- lua/simi.lua
+++ lua/simi.lua
@@ -4,7 +4,14 @@ local function search_with_same_name()
 	require("telescope.builtin").find_files({default_text = fileName})
 end
 
+local function search_with_name_under_cursor()
+	M = require("utils")
+	local word = M.get_word_under_cursor()
+	require("telescope.builtin").find_files({default_text = word})
+end
+
 return {
-	search_with_same_name = search_with_same_name
+	search_with_same_name = search_with_same_name,
+	search_with_name_under_cursor = search_with_name_under_cursor 
 }
 
